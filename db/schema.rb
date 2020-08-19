2134e18442c2a596c6e7af44557afd58429594e9
nekosakana <kiri33pudding@gmail.com>
2020-08-19 16:35:09 +0900

    4-2.データの内容を制限する

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_17_142916) do
+ActiveRecord::Schema.define(version: 2020_08_19_073152) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "tasks", force: :cascade do |t|
-    t.string "name"
+    t.string "name", limit: 30, null: false
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
